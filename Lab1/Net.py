621a9a10e78eba1ee95652eada6aa708933dc622
physics <3150105638@zju.edu.cn>
2020-01-26 20:09:08 +0100

    update

--- Lab1/Net.py
+++ Lab1/Net.py
@@ -5,7 +5,6 @@ from tqdm import trange
 from abc import ABCMeta, abstractmethod
 
 
-
 class Parameter(object):
     def __init__(self, data, requires_grad, skip_decay=False):
         self.data = data
@@ -17,33 +16,36 @@ class Parameter(object):
     def T(self):
         return self.data.T
 
+
 class SGD(object):
     def __init__(self, parameters, lr, decay=0):
-        self.parameters = [p for p in parameters if p.requires_grad]   
+        self.parameters = [p for p in parameters if p.requires_grad]
         self.lr = lr
         self.decay_rate = 1.0 - decay
 
     def update(self):
         for p in self.parameters:
             if self.decay_rate < 1 and not p.skip_decay: p.data *= self.decay_rate
-           # print("data",p.data,"grad",p.grad)
+            # print("data",p.data,"grad",p.grad)
             p.data -= self.lr * p.grad
 
 
 class MSE:
     def __init__(self):
         pass
+
     def gradient(self):
         return self.a - self.y
-    
+
     def __call__(self, output, target, requires_acc=True):
         self.a = output
-        self.y = np.reshape(target,(-1,1))
-        loss = 0.5*np.multiply(self.a-self.y,self.a-self.y).mean()
+        self.y = np.reshape(target, (-1, 1))
+        loss = 0.5 * np.multiply(self.a - self.y, self.a - self.y).mean()
         if requires_acc:
-            acc = np.sum(np.sign(output)==self.y)/output.shape[0]
-            return loss,acc
+            acc = np.sum(np.sign(output) == self.y) / output.shape[0]
+            return loss, acc
         return loss
+
 
 class Layer():
     @abstractmethod
@@ -53,6 +55,7 @@ class Layer():
     @abstractmethod
     def backward(self, *args):
         pass
+
 
 class Tanh(Layer):
     def forward(self, x):
@@ -64,13 +67,14 @@ class Tanh(Layer):
     def backward(self, eta):
         return np.einsum('...,...,...->...', 1 - self.y, 1 + self.y, eta, optimize=True)
 
+
 class Linear(Layer):
     def __init__(self, shape, requires_grad=True, bias=True, **kwargs):
         '''
         shape = (in_size, out_size)
         '''
-        W = np.random.randn(*shape) * (2 / shape[0]**0.5)
-      #  print(W.shape)
+        W = np.random.randn(*shape) * (2 / shape[0] ** 0.5)
+        #  print(W.shape)
         self.W = Parameter(W, requires_grad)
         self.b = Parameter(np.zeros(shape[-1]), requires_grad) if bias else None
         self.require_grad = requires_grad
@@ -82,15 +86,17 @@ class Linear(Layer):
         return out
 
     def backward(self, eta):
-       if self.require_grad:
+        if self.require_grad:
             batch_size = eta.shape[0]
             self.W.grad = np.dot(self.x.T, eta) / batch_size
             if self.b is not None: self.b.grad = np.sum(eta, axis=0) / batch_size
-       return np.dot(eta, self.W.T)
+        return np.dot(eta, self.W.T)
+
+
 class Net(Layer):
     def __init__(self, layer_configures):
         self.layers = []
-        self.parameters=[]
+        self.parameters = []
         for config in layer_configures:
             self.layers.append(self.createLayer(config))
 
@@ -111,38 +117,45 @@ class Net(Layer):
         for layer in self.layers:
             x = layer.forward(x)
         return x
-    
+
     def backward(self, eta):
         for layer in self.layers[::-1]:
             eta = layer.backward(eta)
         return eta
-    
-    def train(self,X,Y,optimizer,batch_size=16,epochs=500,loss=MSE()):
+
+    def train(self, X, Y, optimizer, batch_size=16, epochs=500, loss=MSE()):
         n = len(Y)
         for epoch in trange(epochs):
-            i = 0 
-            while i<= n-batch_size:
-                x,y=X[i:i+batch_size,],Y[i:i+batch_size,]
-                i+=batch_size
+            i = 0
+            while i <= n - batch_size:
+                x, y = X[i:i + batch_size, ], Y[i:i + batch_size, ]
+                i += batch_size
                 output = self.forward(x)
-                batch_loss, batch_acc = loss(output,y)
+                batch_loss, batch_acc = loss(output, y)
                 eta = loss.gradient()
                 self.backward(eta)
                 optimizer.update()
-                if epoch % 100==0:
-                    print("epoch: %d, batch: %5d, batch_acc:    %.2f,batch loss: %.2f" % \
-                    (epoch, i/batch_size,batch_acc*100,batch_loss))
+                # if epoch % 100==0:
+                # print("epoch: %d, batch: %5d, batch_acc:    %.2f,batch loss: %.2f" % \
+                # (epoch, i/batch_size,batch_acc*100,batch_loss))
+        print("epoch: %d, batch: %5d, batch_acc:    %.2f,batch loss: %.2f" % \
+              (epoch, i / batch_size, batch_acc * 100, batch_loss))
+
+    def predict(self, X):
+        return self.forward(X)
+
+
 '''
-layers =[
-    {'type':'linear','shape':(2,1)},
-    {'type':'tanh'},
-    {'type':'linear','shape':(1,1)},
-    {'type':'tanh'},
-    {'type':'linear','shape':(1,1)},
-    {'type':'tanh'}
+layers = [
+    {'type': 'linear', 'shape': (2, 1)},
+    {'type': 'tanh'},
+    {'type': 'linear', 'shape': (1, 1)},
+    {'type': 'tanh'},
+    {'type': 'linear', 'shape': (1, 1)},
+    {'type': 'tanh'}
 ]
 net = Net(layers)
-opt = SGD(net.parameters,lr=1e-3)
-net.train(X,Y,opt)
+opt = SGD(net.parameters, lr=1e-3)
+net.train(X, Y, opt)
 
 '''
